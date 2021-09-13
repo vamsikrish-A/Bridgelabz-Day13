37591a5b6a1edf1c719c394ffec6111648787c88
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-13 14:58:11 +0530

    using Generic Method

--- src/com/bridgelabz/GenericsMaximumTest.java
+++ src/com/bridgelabz/GenericsMaximumTest.java
@@ -44,6 +44,16 @@ public class GenericsMaximumTest {
         return  max;
 
     }
+    //Generic method to find the maximum of three number
+    public static <T extends Comparable<T>> T maximumWithGenerics(T x, T y, T z) {
+        T max = x; //Assuming x is the initially largest
+
+        if (y.compareTo(max) > 0)
+            max = y; //y is the largest so far
+        if (z.compareTo(max) > 0)
+            max = z; //z is the largest
+        return max;
+    }
 
     public static void main(String[] args) {
         System.out.println(" `Welcome to Generics` ");
@@ -56,7 +66,15 @@ public class GenericsMaximumTest {
         //calling out String method.
         String maximumStringVariable = maximumString("Apple","Cherry","Peach");
         System.out.println("maximum String variable: "+maximumStringVariable);
+        //Generic Method
+        System.out.println("======/Finding the Maximum number with Generic Method./====");
+        int maxGenericInt = maximumWithGenerics(6,7,8);
+        double maxGenericDouble = maximumWithGenerics(6.6,7.7,5.5);
+        String maxGenericString = maximumWithGenerics("Apple","Banana","Peach");
+        System.out.println("maximum Integer number with Generic method is: "+maxGenericInt);
+        System.out.println("maximum Float number with Generic method is: "+maxGenericDouble);
+        System.out.println("maximum String with Generic method is: "+maxGenericString);
+
     }
-
-
+    
 }
