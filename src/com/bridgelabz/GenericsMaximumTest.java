f85903e0b1d1057b05de31ba170b4a8f3b3023cb
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-13 09:28:42 +0530

    Maximum float Number

--- src/com/bridgelabz/GenericsMaximumTest.java
+++ src/com/bridgelabz/GenericsMaximumTest.java
@@ -13,12 +13,27 @@ public class GenericsMaximumTest {
         return  max;
 
     }
+    public static <Float extends Comparable<Float>> Float maximumFloat(Float x, Float y, Float z) {
+        Float max = x; //Assuming x is initially the largest
+
+        if (y.compareTo(max) > 0)
+            max = y; // y is the largest so far
+
+        if (z.compareTo(max) > 0)
+            max = z; // z is the largest
+
+        return  max;
+
+    }
 
     public static void main(String[] args) {
         System.out.println(" `Welcome to Generics` ");
-        //call out function
+        //calling out  Integer method
         int maximumIntNumber = maximum(6,7,5);
-        System.out.println(maximumIntNumber);
+        System.out.println("maximum integer number:"+maximumIntNumber);
+        //calling out Float method
+        double maximumFloatNumber = maximumFloat(6.6, 7.7, 5.5);
+        System.out.println("maximum float number:"+maximumFloatNumber);
     }
 
 
