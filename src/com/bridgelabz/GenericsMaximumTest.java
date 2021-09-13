e1aa51a63adb120e68628d24ac7daf7608a39088
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-13 09:16:48 +0530

    Maximum Integer Number

--- src/com/bridgelabz/GenericsMaximumTest.java
+++ src/com/bridgelabz/GenericsMaximumTest.java
@@ -1,8 +1,24 @@
 package com.bridgelabz;
 
 public class GenericsMaximumTest {
+    public static <Integer extends Comparable<Integer>> Integer maximum(Integer x, Integer y, Integer z) {
+        Integer max = x; //Assuming x is initially the largest
+
+        if (y.compareTo(max) > 0)
+            max = y; // y is the largest so far
+
+        if (z.compareTo(max) > 0)
+            max = z; // z is the largest
+
+        return  max;
+
+    }
+
     public static void main(String[] args) {
         System.out.println(" `Welcome to Generics` ");
+        //call out function
+        int maximumIntNumber = maximum(6,7,5);
+        System.out.println(maximumIntNumber);
     }
 
 
